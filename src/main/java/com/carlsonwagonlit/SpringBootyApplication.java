0830967d841de8b67b1f37ffda19ef76e9ebe0e3
joeyt <joeyt@slalom.com>
2017-04-10 14:10:44 -0700

    Untouched output from Spring Initializer
    https://start.spring.io/

---
+++ src/main/java/com/carlsonwagonlit/SpringBootyApplication.java
@@ -0,0 +1,12 @@
+package com.carlsonwagonlit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootyApplication.class, args);
+	}
+}
